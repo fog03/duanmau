9abfe9b167bee17cadfac4ac3f2e549fe5a531f1
fog03 <tranvanphong18102003@gmail.com>
2022-10-04 16:52:40 +0700

    themmodanhmuc

---
+++ admin/danhmuc/add.php
@@ -0,0 +1,23 @@
+<div class="row">
+            <div class="row frmtitle">
+                <h1>THÊM MỚI HÀNG HÓA</h1>
+            </div>
+            <div class="row frmcontent">
+                <form action="" method="post">
+                    <div class="row mb10">
+                        Mã loại <br>
+                        <input type="text" name="maloai" disabled>
+                    </div>
+                    <div class="row mb10">
+                        Tên loại <br>
+                        <input type="text" name="tenloai" >
+                    </div>
+                    <div class="row mb10">
+                        <input type="submit" value="Thêm Mới">
+                        <input type="reset" value="Nhập lại">
+                        <a href="danhsach_loaihang.html"><input type="button" value="DANH SÁCH"></a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>

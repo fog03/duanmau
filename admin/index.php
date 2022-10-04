3c634e9c5fb5894e3262968c3cff548c1b80d58a
fog03 <tranvanphong18102003@gmail.com>
2022-10-04 19:17:17 +0700

    update

--- admin/index.php
+++ admin/index.php
@@ -28,6 +28,25 @@ if (isset($_GET["act"])) {
             $listdanhmuc = pdo_query($sql);
             include "danhmuc/list.php";
             break;
+        case 'suadm':
+            if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+                $sql = "SELECT * FROM danhmuc WHERE id =" .$_GET['id'];
+                $dm=pdo_query_one($sql);
+            }
+            include "danhmuc/update.php";
+            break;
+        case 'updatedm':
+            if (isset($_POST['capnhat']) && ($_POST['capnhat'])) {
+                $tenloai = $_POST['tenloai'];
+                $id = $_POST['id'];
+                $sql = "UPDATE danhmuc set name='".$tenloai."' where id = ".$id;
+                pdo_execute($sql);
+                $thongbao = "Cập nhật Thành công";
+            }
+            $sql = "SELECT * FROM danhmuc order by id desc";
+            $listdanhmuc = pdo_query($sql);
+            include "danhmuc/list.php";
+            break;
         default:
             include "home.php";
             break;
